763420a3a0fb9485e961830e4d7011351377f85f
Sergiy Dmytrenko <dmytrenko.serhii@gmail.com>
2017-02-23 16:40:36 +0200

    Dispatching our events and updating our store

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class App extends Component {
+
+  addTrack(){
+    console.log('addTrack', this.trackInput.value);
+    this.props.onAddTrack(this.trackInput.value);
+    this.trackInput.value = ''
+  }
+
   render() {
     console.log(this.props.testStore);
     return (
     <div>
-      <input type="text"/>
-      <button>Add track</button>
+      <input type="text" ref={(input) => {this.trackInput = input}} /> 
+      <button onClick={this.addTrack.bind(this)}>Add track</button>
       <ul>
         {this.props.testStore.map((track, index) =>
           <li key={index}> {track} </li>
@@ -18,9 +25,17 @@ class App extends Component {
   }
 }
 
+// ref={(input) => {this.trackInput = input} - используем ref, чтобы не слушать локальный state
+// когда проходит рендер в this.trackInput присв ссылка на DOM елемент imput
+
 export default connect(  // декоратор с редакс
   state => ({
     testStore: state //состояние стора с ф-и playlist (store)
   }), // mapStateToProps она мапит стейт с стора(состояния) в пропс реакт компонента
-  dispatch => ({})
+  dispatch => ({
+    onAddTrack: (trackName) => {
+      dispatch({ type: 'ADD_TRACK', payload: trackName })
+    }
+  })
+  // диспатчим ивент по клику
 )(App);
